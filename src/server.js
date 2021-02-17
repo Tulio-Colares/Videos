7e5bb7d53afda36e0d155e07cf3bd4bc3baced2c
Tulio Colares <tuliocsaraiva@gmail.com>
2021-02-17 18:40:30 -0300

    alterei o pakage.json

--- src/server.js
+++ src/server.js
@@ -5,13 +5,14 @@ const app = express();
 app.use('/', 
 express.static(resolve(__dirname, './build')))
 
-//ATENÇÃO!! Lembre-se sempre de: 
-//1 - Instalar o express
-//2 - Executar o comando de build (npm run-script build) no terminal
-//3 - criar esta pasta (server.js) dentro de src, no mesmo nivel do index.js, e copiar o conteúdo daqui pr ela
-
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {return console.log(err) }
 
   console.log('Tudo funcionando certinho')
 })
+
+//ATENÇÃO!! Lembre-se sempre de: 
+//1 - Instalar o express
+//2 - Executar o comando de build (npm run-script build) no terminal
+//3 - Vá em package.json e coloquei em "scripts" -> <"start": "node index.js",> com a vírgula no final
+//4 - criar esta pasta (server.js) dentro de src, no mesmo nivel do index.js, e copiar o conteúdo daqui pr ela
