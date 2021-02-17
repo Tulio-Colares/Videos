da6152217b53a60b1e22b5593824af622fb6df09
Tulio Colares <tuliocsaraiva@gmail.com>
2021-02-17 18:10:28 -0300

    fiz o build

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const app = express();
 app.use('/', 
 express.static(resolve(__dirname, './build')))
 
+//ATENÇÃO!! Lembre-se sempre de executar no terminal o comando de build (npm run-script build)
+
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {return console.log(err) }
 
