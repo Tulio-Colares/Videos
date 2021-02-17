3cf092e9c6d2e310eb2eaa5eb622103e27fd302a
Tulio Colares <tuliocsaraiva@gmail.com>
2021-02-17 18:16:21 -0300

    baixei o express

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ const app = express();
 app.use('/', 
 express.static(resolve(__dirname, './build')))
 
-//ATENÇÃO!! Lembre-se sempre de executar no terminal o comando de build (npm run-script build)
+//ATENÇÃO!! Lembre-se sempre de: 
+//1 - Instalar o express
+//2 - Executar o comando de build (npm run-script build) no terminal
+//3 - criar esta pasta (server.js) dentro de src, no mesmo nivel do index.js, e copiar o conteúdo daqui pr ela
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {return console.log(err) }
